05203313aa9608f3423abcc46587ac8992eb52a4
selinavs <selinasdot@yahoo.com>
2018-05-09 18:50:29 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
   production: false,
-  speechServerUrl: 'ws://localhost:38766',
+  speechServerUrl: 'https://www.google.com/speech-api/v1/recognize',
 
   gCloudProjectId: 'speechwithcloudstorage',
   gCloudProjectApiKey: '31e0121ccd54339bc906d59ba6a5cc962a984504',
