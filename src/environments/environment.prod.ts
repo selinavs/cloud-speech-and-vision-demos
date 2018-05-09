a3f9032d7d22c962115f314445d5850b854dae90
selinavs <selinasdot@yahoo.com>
2018-05-09 16:44:36 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
-  speechServerUrl: 'ws://localhost:8000',
+  speechServerUrl: 'ws://localhost:38766',
 
-  gCloudProjectId: 'ENTER YOUR GLCOUD PROJECT ID HERE',
-  gCloudProjectApiKey: 'ENTER YOUR OWN API KEY HERE',
+gCloudProjectId: 'speechwithcloudstorage',
+  gCloudProjectApiKey: '31e0121ccd54339bc906d59ba6a5cc962a984504',
 
   gCloudNLPApiUrl: 'https://language.googleapis.com/v1/documents:annotateText',
   gCloudVisionApiUrl: 'https://vision.googleapis.com/v1/images:annotate',
-};
+}; 
