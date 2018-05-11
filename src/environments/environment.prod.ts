aa8e0ccf9de612c6fce34f95e06d4e7b22822b56
selinavs <selinasdot@yahoo.com>
2018-05-11 12:30:01 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true,
   speechServerUrl: 'https://www.google.com/speech-api/v1/recognize',
 
-gCloudProjectId: 'speechwithcloudstorage',
-  gCloudProjectApiKey: '31e0121ccd54339bc906d59ba6a5cc962a984504',
+gCloudProjectId: 'speechwithwebappgridcell',
+  gCloudProjectApiKey: '35e2b35ae966247efd36b2ab924dd00269efde1d',
 
   gCloudNLPApiUrl: 'https://language.googleapis.com/v1/documents:annotateText',
   gCloudVisionApiUrl: 'https://vision.googleapis.com/v1/images:annotate',
